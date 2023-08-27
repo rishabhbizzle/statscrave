83a82a117c7aab0231daf42b7970a11867662e29
rishabhbizzle <rishabhbizzle@gmail.com>
2023-08-27 14:19:00 +0530

    sending streaming data from our db in response from api

--- src/app/api/album/[id]/route.js
+++ src/app/api/album/[id]/route.js
@@ -1,17 +1,21 @@
 import Spotify from "@/helper/spotify";
 import { NextResponse } from "next/server";
+import { connect } from "@/dgConfig/dbConfig";
+import Album from "@/models/albumModel";
 
 export const GET = async (request, { params }) => {
     try {
+        await connect();
         const { id } = params;
         const albumDetails = await Spotify.albums.get(id, "ES")
         const albumTracks = await Spotify.albums.tracks(id, "ES", 50)
-        // const relatedArtists = await Spotify.artists.relatedArtists(id);
+        const streamingData = await Album.findOne({ spotifyId: id })
         return NextResponse.json({
             success: true,
             data: {
                 albumDetails,
                 albumTracks,
+                streamingData
             },
             message: "Album found successfully"
         })
--- src/app/api/artist/[id]/route.js
+++ src/app/api/artist/[id]/route.js
@@ -1,14 +1,18 @@
 import Spotify from "@/helper/spotify";
 import { NextResponse } from "next/server";
+import { connect } from "@/dgConfig/dbConfig";
+import Artist from "@/models/artistModel";
 
 export const GET = async (request, { params }) => {
     try {
+        await connect();
         const { id } = params;
         const artist = await Spotify.artists.get(id)
         const albums = await Spotify.artists.albums(id, "album", "ES", 30);
         const singles = await Spotify.artists.albums(id, "single", "ES", 20);
         const topTracks = await Spotify.artists.topTracks(id, "ES");
         // const relatedArtists = await Spotify.artists.relatedArtists(id);
+        const streamingData = await Artist.findOne({ spotifyId: id })
         return NextResponse.json({
             success: true,
             data: {
@@ -16,6 +20,7 @@ export const GET = async (request, { params }) => {
                 albums,
                 singles,
                 topTracks,
+                streamingData
                 // relatedArtists
             },
             message: "Artist found successfully"
