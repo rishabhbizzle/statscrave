dcc38a740aa3577f0dcc5d10ff8456e8b1d940f3
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-01 21:20:48 +0530

    feat: most popular songs section of artist

---
+++ src/components/artist/populars.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { getArtistMostPopularSongs } from "@/lib/actions";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { millisecondsToMinutesSeconds } from "@/lib/helperFunctions";
+const PopularTracks = async ({ id }) => {
+  const tracks = await getArtistMostPopularSongs(id);
+  return (
+    <div className="md:px-10 my-5">
+      <Card>
+        <CardHeader>
+          <CardTitle>Current Popular Tracks</CardTitle>
+          <CardDescription className="text-muted-foreground">
+            The rankings is calculated by algorithm and is based, in the most
+            part, on the total number of plays the track has had and how recent
+            those plays are. Generally speaking, songs that are being played a
+            lot now will have a higher popularity than songs that were played a
+            lot in the past.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[5%]">#</TableHead>
+                <TableHead className="w-[20%]">Cover</TableHead>
+                <TableHead className="w-[55%]">Title</TableHead>
+                <TableHead className="w-[10%]">Duration</TableHead>
+                <TableHead className="text-right">Popularity</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {tracks.map((track, idx) => (
+                <TableRow key={idx}>
+                  <TableCell>{idx + 1}</TableCell>
+                  <TableCell>
+                    <Image
+                      alt="cover"
+                      className="object-cover"
+                      height="50"
+                      src={track.album.images[0].url}
+                      width="50"
+                    />
+                  </TableCell>
+                  <TableCell>{track.name}</TableCell>
+                  <TableCell>
+                    {millisecondsToMinutesSeconds(track.duration_ms)}
+                  </TableCell>
+
+                  <TableCell className="text-right">
+                    {track.popularity}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default PopularTracks;
--- src/lib/helperFunctions.js
+++ src/lib/helperFunctions.js
@@ -1,8 +1,27 @@
 export const millisecondsToMinutesSeconds = (duration_ms) => {
-    let seconds = Math.floor(duration_ms / 1000);
-    let minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
-    // Format the result as minutes:seconds
-    let formattedDuration = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-    return formattedDuration;
+  let seconds = Math.floor(duration_ms / 1000);
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  // Format the result as minutes:seconds
+  let formattedDuration = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  return formattedDuration;
+}
+
+export const getLatestDateValue = (data) => {
+  let maxDate = null;
+  let maxValue = null;
+
+  for (const dateStr in data) {
+    // Parsing the date string in the format "DD-MM-YYYY"
+    const parts = dateStr.split('-');
+    const currentDate = new Date(`${parts[1]}-${parts[0]}-${parts[2]}`);
+    const value = data[dateStr];
+
+    if (!isNaN(currentDate.getTime()) && (maxDate === null || currentDate > maxDate)) {
+      maxDate = currentDate;
+      maxValue = value;
+    }
   }
+  console.log(maxValue);
+  return maxValue;
+}
