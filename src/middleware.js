85333c715bb6ac4e0e8298a538634a5a5dd0b9ba
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-04 21:28:08 +0530

    auth middleware added

---
+++ src/middleware.js
@@ -0,0 +1,7 @@
+import {withAuth} from "@kinde-oss/kinde-auth-nextjs/middleware";
+export default function middleware(req) {
+    return withAuth(req);
+}
+export const config = {
+    matcher: ["/dashboard"]
+};
