be10af5fe542479a3cadbb2fb439d8d6c9276420
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-04 18:46:32 +0530

    feat: track page

--- src/app/track/[id]/page.js
+++ src/app/track/[id]/page.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import Container from '@/components/ui/container'
+import StreamingDetails from '@/components/StreamingDetails'
+import BasicDetails from '@/components/BasicDetails'
+import { Recomendations } from '@/components/recomendations'
+import { getTrackData, isUserFavorite } from '@/lib/actions'
+import Loader from '@/components/ui/loader'
 
-const Page = () => {
+
+const Track = async ({ params }) => {
+  const id = params.id
+  const { trackDetails, streamingData } = await getTrackData(id)
+  const isFavourite = await isUserFavorite('track', id)
+
   return (
-    <div>P</div>
+    <Container>
+      <BasicDetails details={trackDetails} type='track' isFavourite={isFavourite} />
+      <div className="flex-1 space-y-4 pt-6">
+        {streamingData && (
+          <Suspense fallback={<Loader />}>
+
+            <StreamingDetails streamingData={streamingData} type='track' />
+          </Suspense>
+        )}
+        {/* <Suspense fallback={<div>Loading...</div>}>
+          <OtherDetails details={trackDetails} type='album' />
+        </Suspense> */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Recomendations type='track' />
+        </Suspense>
+      </div>
+    </Container>
   )
 }
 
-export default Page
+export default Track
