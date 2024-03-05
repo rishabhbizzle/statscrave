9b8282bb5b1cd8f75d689259c3c57f4ab5c2e016
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-06 00:35:08 +0530

    font change

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,12 @@ import Navbar from '@/components/navbar'
 import './globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import Footer from '@/components/footer'
+import { Poppins, Roboto } from 'next/font/google'
+ 
+const roboto = Poppins({
+  weight: ["400", "500", "700", "900"],
+  subsets: ['latin'],
+})
 
 
 export const metadata = {
@@ -15,7 +21,7 @@ export default function RootLayout({ children }) {
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body>
+        <body className={roboto.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
