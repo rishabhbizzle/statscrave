786e7de1e951e28612e40aa8f33898847ccd6293
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-06 00:56:24 +0530

    revert

--- src/helper/spotify.js
+++ src/helper/spotify.js
@@ -6,12 +6,12 @@ const Spotify = SpotifyApi.withClientCredentials(
     process.env.SPOTIFY_CLIENT_SECRET
 );
 
-Spotify.authenticate().then(async (data) => {
-    const token = await Spotify.getAccessToken();
-    console.log(`Spotify token: ${token}`);
-}).catch((err) => {
-    console.log(err)
-})
+// Spotify.authenticate().then(async (data) => {
+//     const token = await Spotify.getAccessToken();
+//     console.log(`Spotify token: ${token}`);
+// }).catch((err) => {
+//     console.log(err)
+// })
 
 console.log("Spotify initialized");
 
