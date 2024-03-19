bbca8cf0d4524c608b231bcbc91aa98253ad3b16
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-19 23:45:27 +0530

    fix: global error message fixed

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -5,7 +5,10 @@ export default function GlobalError({ error, reset }) {
     <html>
       <body>
         <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <p>Sorry, something went wrong. Please send us this error message on our socials and we'll fix it as soon as possible.</p>
+        <p>{error?.message}</p>
+        <pre>{error?.stack}</pre>
+        {/* <button onClick={() => reset()}>Try again</button> */}
       </body>
     </html>
   )
