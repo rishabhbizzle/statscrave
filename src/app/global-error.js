25363ed561b99f0a70e8e6304406dc480b31a4d9
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-20 20:24:35 +0530

    fix: build error

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -5,7 +5,7 @@ export default function GlobalError({ error, reset }) {
     <html>
       <body>
         <h2>Something went wrong!</h2>
-        <p>Sorry, something went wrong. Please send us this error message on our socials and we'll fix it as soon as possible.</p>
+        <p>Sorry, something went wrong. Please send us this error message on our any of our socials and we will fix it as soon as possible</p>
         <p>{error?.message}</p>
         <pre>{error?.stack}</pre>
         {/* <button onClick={() => reset()}>Try again</button> */}
