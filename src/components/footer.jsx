a0426aa002d60f4beabfa4513c7a40b5f4748a44
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-22 21:34:56 +0530

    fix: footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -24,21 +24,20 @@ export default function Footer() {
     <footer className="border-t border-gray-700 mt-10">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between flex-col gap-5">
-            <div className="flex justify-between">
-          <div className="flex space-x-6">
-
-            <p className="text-muted-foreground">StatsCrave</p>
-            <p className="text-sm ">© 2024</p>
-            <Badge variant="secondary">BETA</Badge>
+          <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-2">
+            <div className="flex justify-center items-center space-x-6">
+              <p className="text-muted-foreground">StatsCrave</p>
+              <p className="text-sm ">© 2024</p>
+              <Badge variant="secondary">BETA</Badge>
             </div>
             <div className="flex items-center space-x-4">
-              <Link href="/" target="_blank" rel="noreferrer">
+              {/* <Link href="/" target="_blank" rel="noreferrer">
                 <div className={cn("w-9 px-0")}>
                   <Github className="h-4 w-4" />
                   <span className="sr-only">GitHub</span>
                 </div>
-              </Link>
-              <Link href="/" target="_blank" rel="noreferrer">
+              </Link> */}
+              <Link href='https://twitter.com/jdbsdata' target="_blank" rel="noreferrer">
                 <div className={cn("w-9 px-0")}>
                   <Twitter className=" h-5 w-5 fill-current" />
                   <span className="sr-only">Twitter</span>
@@ -49,12 +48,12 @@ export default function Footer() {
           </div>
           <div className="flex flex-col-reverse justify-col items-center">
             <div className="flex flex-col gap-2">
-              <div className="flex space-x-6 text-muted-foreground text-sm">
-                We are not related to Billboard, Spotify or any of its partners. This is a fan project. The data shown on this website is publicly available and
-                retrieved directly from APIs.
+              <div className="flex space-x-6 text-muted-foreground text-sm text-center">
+                We are not related to Billboard, Spotify or any of its partners.
+                This is a fan project. The data shown on this website is
+                publicly available and retrieved directly from APIs.
               </div>
             </div>
-            
           </div>
         </div>
       </div>
