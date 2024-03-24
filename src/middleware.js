2d086f17fff4405a3c39093627b8c70267588f2b
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-24 15:02:32 +0530

    fix: middleware issue

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,6 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
-export default authMiddleware({
-    afterAuth(auth, req, evt) {
-        // Handle users who aren't authenticated
-        if (!auth.userId) {
-            return redirectToSignIn();
-        }
-    }
-});
+export default authMiddleware();
 
 export const config = {
     matcher: ["/dashboard", "/user-profile"],
