30233923fbfef881c6b6467d403c3340759ec972
rishabhbizzle <rishabhbizzle@gmail.com>
2024-05-07 21:15:05 +0530

    [feat] lastFm top tracks page with countries specific chart

---
+++ src/app/charts/[name]/loading.js
@@ -0,0 +1,12 @@
+import Loader from '@/components/ui/loader'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='min-h-screen'>
+      <Loader />
+    </div>
+  )
+}
+
+export default loading
--- src/app/charts/page.js
+++ src/app/charts/page.js
@@ -12,26 +12,32 @@ const ChartsPage = () => {
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 text-center py-10 px-4 md:px-8 text-white">
         <Link href="/charts/hot100">
         <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 m-4 bg-black">
-          <img className="mx-auto" src='./head.jpg' alt="HOT100" />
-          <p className="pt-9 pb-1 ">Billboard Hot 100</p>
+          <img className="mx-auto w-full h-32 object-cover" src='./head.jpg' alt="HOT100" />
+          <p className="my-3 ">Billboard Hot 100</p>
         </div>
         </Link>
         <Link href="/charts/global200">
         <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 m-4 bg-black">
-          <img className="mx-auto" src="./global.jpg" alt="GLOBAL200" />
+          <img className="mx-auto w-full h-32 object-cover" src="./global.jpg" alt="GLOBAL200" />
           <p className="my-3 ">Billboard Global 200</p>
         </div>
         </Link>
         <Link href="/charts/album200">
         <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 m-4 bg-black">
-          <img className="mx-auto" src="./album.png" alt="BB200" />
+          <img className="mx-auto w-full h-32 object-cover" src="./album.png" alt="BB200" />
           <p className="my-3">Billboard 200 (Albums)</p>
         </div>
         </Link>
         <Link href="/charts/artist100">
         <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 m-4 bg-black">
-          <img className="mx-auto" src="./artist.png" alt="BB200" />
+          <img className="mx-auto w-full h-32 object-cover" src="./artist.png" alt="BB200" />
           <p className="my-3">Billboard Artist 100</p>
+        </div>
+        </Link>
+        <Link href="/charts/lastFmTopTracks">
+        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 m-4 bg-black">
+          <img className="mx-auto w-full h-32 object-cover" src="https://www.last.fm/static/images/lastfm_logo_facebook.15d8133be114.png" alt="BB200" />
+          <p className="my-3">Last Fm Top Tracks</p>
         </div>
         </Link>
       </div>
