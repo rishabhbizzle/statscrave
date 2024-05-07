d6a0ad4a0e0fb5bf834757da9e86873468d24c52
rishabhbizzle <rishabhbizzle@gmail.com>
2024-05-07 21:13:29 +0530

    [feat] get countires server action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -9,7 +9,7 @@ import { revalidatePath } from "next/cache";
 export const getAllBlogsFromDb = async (count) => {
     try {
         await connect()
-        const blogs = count ? await Updates.find({}).limit(count).sort({ createdAt: -1 }): await Updates.find({}).sort({ createdAt: -1 })
+        const blogs = count ? await Updates.find({}).limit(count).sort({ createdAt: -1 }) : await Updates.find({}).sort({ createdAt: -1 })
 
         // convert the blogs to json
         const data = JSON.parse(JSON.stringify(blogs))
@@ -40,7 +40,7 @@ export const getBlogPostFromDb = async (slug) => {
 export const createBlogPostInDb = async (blog) => {
     try {
         const user = await currentUser();
-        console.log(user, user?.firstName, user?.id )
+        console.log(user, user?.firstName, user?.id)
         if (!user) {
             return { data: null, error: 'User not found' }
         }
@@ -69,12 +69,27 @@ export const createBlogPostInDb = async (blog) => {
     }
 }
 
-export const getLastFmTopTracks = async (page = 1, limit = 10) => {
+export const getLastFmTopTracks = async (page = 1, limit = 10, country) => {
     try {
-        const data = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=ecc9b2c8a479d81dec5c6a6baadf3c09&format=json&page=${page}&limit=${limit}`)
+        const data = await axios.get(country ? `http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=ecc9b2c8a479d81dec5c6a6baadf3c09&format=json&page=${page}` : `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=ecc9b2c8a479d81dec5c6a6baadf3c09&format=json&page=${page}`)
         return data?.data?.tracks
     } catch (error) {
         console.error(error);
         return null
+    }
+}
+
+export const getAllCountries = async () => {
+    try {
+        const data = await fetch("https://restcountries.com/v3.1/all")
+            .then((response) => response.json())
+            .then((data) => {
+                const countryNames = data.map((country) => country.name.common);
+                return countryNames;
+            })
+        return data;
+    } catch (error) {
+        console.error(error)
+        return []
     }
 }
