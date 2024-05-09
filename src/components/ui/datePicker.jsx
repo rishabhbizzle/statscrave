870530b9b30718fc279aeb88716311d33dedc3aa
rishabhbizzle <rishabhbizzle@gmail.com>
2024-05-09 20:34:18 +0530

    [feat] datepicker component added

---
+++ src/components/ui/datePicker.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useRouter } from "next/navigation";
+
+export function DatePicker({ otherParams = [] }) {
+  const [date, setDate] = React.useState();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (date) {
+      router.push(`?date=${format(date, "MM-dd-yyyy")}&${otherParams.map((param) => `${param.key}=${param.value}`).join("&")}`, {
+        scroll: false,
+      });
+    }
+  }, [date]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
