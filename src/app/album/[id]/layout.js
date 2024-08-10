7c56de7fe8fff12bf21e6d587ee7e85d03f7903a
rishabhbizzle <rishabhbizzle@gmail.com>
2024-08-10 23:27:26 +0530

    fix: dynamic metadata in artists, albums & tracks page

---
+++ src/app/album/[id]/layout.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const id = params.id
+  // fetch data
+  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/album/${id}?metaData=true`, { cache: 'force-cache'}).then((res) => res.json())
+  // optionally access and extend (rather than replace) parent metadata
+  return {
+    title: `${data?.data?.albumDetails?.name} by ${data?.data?.albumDetails?.artists[0]?.name || ''} | StatsCrave`,
+    description: data?.data?.albumDetails?.summary || "A platform which offers comprehensive streaming statistics and personalized dashboard. Track trends, discover new music, and stay updated on pop culture developments effortlessly.",
+    openGraph: {
+      images: [
+        { url: data.data?.albumDetails?.images[0]?.url, alt: data.data?.albumDetails?.name },
+        ],
+      title: `${data?.data?.albumDetails?.name} by ${data?.data?.albumDetails?.artists[0]?.name || ''} | StatsCrave`,
+      description: data.data?.albumDetails?.summary || "A platform which offers comprehensive streaming statistics and personalized dashboard. Track trends, discover new music, and stay updated on pop culture developments effortlessly.",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${data?.data?.albumDetails?.name} by ${data?.data?.albumDetails?.artists[0]?.name || ''} | StatsCrave`,
+      url: 'https://statscrave.com',
+      description: "A platform which offers comprehensive streaming statistics and personalized dashboard. Track trends, discover new music, and stay updated on pop culture developments effortlessly.",
+      creator: "@StatsCrave",
+      images: [
+        {
+          url: data?.data?.albumDetails?.images[0]?.url,
+          alt: data?.data?.albumDetails?.name,
+        },
+        {
+          url: 'https://i.postimg.cc/C10cs3Sf/og-image-wide.png',
+          alt: 'StatsCrave - Your ultimate music analytics platform!!',
+        },
+      ],
+    },
+  }
+}
+
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
--- src/app/artist/[id]/layout.js
+++ src/app/artist/[id]/layout.js
@@ -1,9 +1,47 @@
 import React from 'react'
 
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const id = params.id
+  // fetch data
+  const artistData = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/artist/${id}`, { cache: 'force-cache'}).then((res) => res.json())
+  // optionally access and extend (rather than replace) parent metadata
+  return {
+    title: `${artistData?.data?.name} | StatsCrave`,
+    description: artistData.data?.summary,
+    openGraph: {
+      images: [
+        { url: artistData.data?.images[0]?.url, alt: artistData.data?.name },
+        ],
+      title: `${artistData?.data?.name} | StatsCrave`,
+      description: artistData.data?.summary,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${artistData?.data?.name} | StatsCrave`,
+      url: 'https://statscrave.com',
+      description: "A platform which offers comprehensive streaming statistics and personalized dashboard. Track trends, discover new music, and stay updated on pop culture developments effortlessly.",
+      creator: "@StatsCrave",
+      images: [
+        {
+          url: artistData.data?.images[0]?.url,
+          alt: artistData.data?.name,
+        },
+        {
+          url: 'https://i.postimg.cc/C10cs3Sf/og-image-wide.png',
+          alt: 'StatsCrave - Your ultimate music analytics platform!!',
+        },
+      ],
+    },
+  }
+}
+
+
 const ArtistLayout = ({ children }) => {
   return (
     <div>
-        {children}
+      {children}
     </div>
   )
 }
