d306cee8546a42e7616a90ac94319985b3afdc82
rishabhbizzle <rishabhbizzle@gmail.com>
2024-08-26 22:26:10 +0530

    feat: new hero section

--- src/components/landing-page/hero-section.jsx
+++ src/components/landing-page/hero-section.jsx
@@ -1,13 +1,20 @@
 "use client";
 
 import React from "react";
-import { ArrowBigDownDash, CheckIcon, FileText } from "lucide-react";
+import {
+  ArrowBigDownDash,
+  ArrowUpRight,
+  CheckIcon,
+  FileText,
+} from "lucide-react";
 import { AnimatePresence, motion, useInView } from "framer-motion";
 import { useRef } from "react";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import { SignIn, SignInButton, SignedOut } from "@clerk/nextjs";
+import { TwitterBorder } from "./TwitterLink";
+import PlatformsSlider from "./PlatformSlider";
 
 const HeroSection = () => {
   const FADE_DOWN_ANIMATION_VARIANTS = {
@@ -17,77 +24,92 @@ const HeroSection = () => {
 
   const heroSectionRef = useRef(null);
   const heroSectionInView = useInView(heroSectionRef);
+  const slap = {
+    initial: {
+      opacity: 0,
+      scale: 1.1,
+    },
+    whileInView: { opacity: 1, scale: 1 },
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+    viewport: { once: true },
+  };
 
   return (
-    <div className="flex min-h-[85vh] p-8  md:mb-14 justify-center items-center">
-      <div className="flex flex-col gap-10 md:gap-2 md:flex-row items-center justify-between">
-        <div className="flex flex-col items-center">
-          <div
-            // initial="hidden"
-            className="max-w-2xl"
-            // ref={heroSectionRef}
-            // animate={heroSectionInView ? "show" : "hidden"}
-            // viewport={{ once: true }}
-            // variants={{
-            //   hidden: {},
-            //   show: {
-            //     transition: {
-            //       staggerChildren: 0.15,
-            //     },
-            //   },
-            // }}
-          >
-            <h1
-              variants={FADE_DOWN_ANIMATION_VARIANTS}
-              className="space-x-4 text-5xl font-bold tracking-tight sm:text-7xl"
-            >
-              <div className="text-primary">StatsCrave</div>
-            </h1>
-            <p
-              variants={FADE_DOWN_ANIMATION_VARIANTS}
-              className="space-x-4 text-xl font-bold tracking-tight sm:text-3xl mt-3"
-            >
-              Your ultimate music analytics platform
-            </p>
-
-            <p
-              variants={FADE_DOWN_ANIMATION_VARIANTS}
-              className="mt-6 text-base sm:text-lg leading-8 "
-            >
-              Dive into our comprehensive streaming analytics to uncover the
-              pulse of your favorite artists, albums, and songs and much more.
-            </p>
-
-            <div className="mt-10 flex items-center  gap-x-6 ">
-              <SignedOut>
-                <SignInButton>
-                  <Button variant="secondary">
-                    <ArrowBigDownDash className="w-5 h-5 mr-2 hover:animate-spin" />
-                    Get Started
-                  </Button>
-                </SignInButton>
-              </SignedOut>
-              <Link href="/updates">
-                <Button variant="outline">
-                  <FileText className="w-5 h-5 mr-2 hover:animate-spin" />
-                  Read Updates
-                </Button>
-              </Link>
-            </div>
-          </div>
+    <>
+    <section className="flex relative flex-col gap-5 justify-center items-center mt-10 max-w-xl md:mt-16 md:max-w-2xl lg:max-w-3xl">
+      <TwitterBorder />
+      <motion.h1
+        {...{
+          ...slap,
+          transition: { ...slap.transition, delay: 0.2 },
+        }}
+        className="text-center  mx-auto bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)]  bg-clip-text text-4xl tracking-tighter  sm:text-5xl dark:text-transparent md:text-6xl lg:text-7xl"
+        >
+        Your ultimate{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-green-500 dark:to-green-200 from-zinc-900 dark:from-zinc-300">
+          music analytics
+        </span>{" "}
+        platform
+      </motion.h1>
+      <motion.p
+        {...{
+          ...slap,
+          transition: { ...slap.transition, delay: 0.3 },
+        }}
+        className="text-lg text-center text-soft-foreground-text"
+        >
+        StatsCrave offers comprehensive streaming statistics of various music giants. Track your top artists, albums & tracks, and stay updated on charts, trends & pop culture.
+      </motion.p>
+      {/* <Link
+        href="/signin"
+        className="inline-flex text-lg gap-x-2 mt-2 backdrop-blur-md text-white justify-center items-center py-3 px-5 w-fit rounded-3xl border duration-200 group bg-page-gradient border-white/30 text-md font-geistSans hover:border-zinc-600 hover:bg-transparent/10 hover:text-zinc-100"
+        >
+        It's free. Sign up now
+        <div className="flex overflow-hidden relative justify-center items-center ml-1 w-5 h-5">
+          <ArrowUpRight className="absolute transition-all duration-500 group-hover:translate-x-4 group-hover:-translate-y-5" />
+          <ArrowUpRight className="absolute transition-all duration-500 -translate-x-4 -translate-y-5 group-hover:translate-x-0 group-hover:translate-y-0" />
         </div>
-        <div className="flex">
-          <img
-            src="/hero1.webp"
-            alt="StatsCrave"
-            loading="lazy"
-            className="rounded-lg"
-            width={500}
-            height={500}
+      </Link> */}
+      <a
+        href="https://www.producthunt.com/posts/statscrave?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-statscrave"
+        target="_blank"
+        >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=480022&theme=dark"
+          alt="StatsCrave - Your&#0032;music&#0032;analytics&#0032;platform | Product Hunt"
+          style={{ width: "250px", height: "54px" }}
+          width="250"
+          height="54"
           />
-        </div>
-      </div>
-    </div>
+      </a>
+     
+    </section>
+    <PlatformsSlider />
+			{/* <div className="relative z-50">
+				<motion.img
+        {...{
+						...slap,
+						transition: { ...slap.transition, delay: 0.35 },
+            }}
+					src="/images/landing-hero.jpeg"
+					alt="Landing page background"
+					width={1512}
+					height={1405}
+					draggable="false"
+					className="z-40 md:mt-[-40px] hidden sm:block h-full max-w-[70vw] mx-auto md:w-full select-none px-5 !rounded-2xl"
+					style={{
+						borderRadius: "20px",
+            }}
+            />
+            <div
+            className="absolute -z-10 left-0 top-[10%] h-32 w-[90%] overflow-x-hidden bg-[rgb(54,157,253)] bg-opacity-100  blur-[337.4px]"
+            style={{ transform: "rotate(-30deg)" }}
+            />
+            </div> */}
+    </>
   );
 };
 
